6c830381966b42fb0dff94e85ed5dfc43e2379fc
thebestKaKa <2429642242@qq.com>
2020-11-30 17:29:21 +0800

    获取书籍json串

--- studywell.py
+++ studywell.py
@@ -4,8 +4,6 @@ from flask import Flask, redirect, request
 
 app = Flask(__name__)
 
-conn = sqlite3.connect("StudyWell.db")
-
 
 @app.route("/index")
 def index():
@@ -15,13 +13,13 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        conn = sqlite3.connect("StudyWell.db")
         username_temp = str(request.form['username'])
         password_temp = str(request.form['password'])
         cursor = conn.cursor()
         user_list = cursor.execute("select * from user where user.username = ?", (username_temp,))
         user_list = user_list.fetchall()
-        ans = ""
-        if (len(user_list)):
+        if len(user_list):
             ans = "the username has been registerd"
         else:
             cursor.execute('insert into user(username,password) values(?,?)', (username_temp, password_temp,))
@@ -37,15 +35,29 @@ def register():
 @app.route('/getbooks', methods=['GET', 'POST'])
 def getbooks():
     if request.method == 'POST':
+        conn = sqlite3.connect("StudyWell.db")
         cursor = conn.cursor()
-        user_list = cursor.execute("select * from user")
-        user_list = user_list.fetchall()
-        ans = ""
-        if (len(user_list)):
-            print(user_list)
-            jsonArr = json.dumps(user_list, ensure_ascii=False)
-            print(json.dumps(dict(user_list)))
-            ans = "the username has been get!"
+        book_list = cursor.execute("select * from book")
+        book_list = book_list.fetchall()
+        book_key = []
+        book_value = []
+        for book in book_list:
+            book_key.append(book[1])
+            book_value_temp = []
+            book_value_temp.append(book[0])
+            book_value_temp.append(book[2])
+            book_value_temp.append(book[3])
+            book_value_temp.append(book[4])
+            book_value.append(book_value_temp)
+        book_dict = dict(zip(book_key, book_value))
+        if len(book_list):
+            # print(book_list)
+            # print(book_dict)
+            # print(json.dumps(book_dict, ensure_ascii=False))
+            print(type(json.dumps(book_dict, ensure_ascii=False)))
+            ans = json.dumps(book_dict, ensure_ascii=False)
+        else:
+            ans = "there is no book!"
         cursor.close()
         conn.close()
         return ans
@@ -54,19 +66,19 @@ def getbooks():
 
 
 @app.route('/upload_books', methods=['GET', 'POST'])
-def getbooks():
+def upload_books():
     if request.method == 'POST':
+        conn = sqlite3.connect("StudyWell.db")
         name_temp = str(request.form['name'])
         id_temp = str(request.form['id'])
         auther_temp = str(request.form['auther'])
         publisher_temp = str(request.form['publisher'])
         publish_time_temp = str(request.form['publish time'])
         cursor = conn.cursor()
-        user_list = cursor.execute('insert into books(name, id, auther, publisher, publish time) '
-                                   'values(?,?,?,?,?)', (name_temp, id_temp, auther_temp, publisher_temp, publish_time_temp))
+        user_list = cursor.execute('insert into books(name, id, auther, publisher, publish time) values(?, ?, ?, ?, ?)',
+                                   (name_temp, id_temp, auther_temp, publisher_temp, publish_time_temp))
         user_list = user_list.fetchall()
         ans = ""
-
         cursor.close()
         conn.close()
         return ans
