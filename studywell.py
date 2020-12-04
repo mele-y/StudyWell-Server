0ede9f2101d7eff22a88abffd50eff84f3b083bc
thebestKaKa <2429642242@qq.com>
2020-12-04 16:00:12 +0800

    增加模糊查询功能 body form名为info

--- studywell.py
+++ studywell.py
@@ -3,8 +3,10 @@ import json
 from flask import Flask, redirect, request, jsonify
 import os
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 import time
+
 
 @app.route("/index")
 def index():
@@ -20,17 +22,137 @@ def login():
         cursor = conn.cursor()
         user = cursor.execute("select * from user where user.username = ?", [username_temp, ]).fetchall()
         if len(user) <= 0:
-            status = '2'
+            code = 2
             msg = 'user does not exist'
             data = []
         else:
             user_password = user[0][1]
             if password_temp == user_password:
-                status = '1'
+                code = 1
                 msg = 'login success'
                 book_list = cursor.execute("select * from book").fetchall()
                 data = []
+                i = 0
                 for book in book_list:
+                    if i < 10:
+                        book_info = {
+                            "book_id": book[1],
+                            "book_name": book[0],
+                            "auther": book[2],
+                            "publcation": book[3],
+                            "book_description": book[4],
+                            "publish_date": book[5],
+                            "upload_date": book[6],
+                        }
+                        data.append(book_info)
+                        i += 1
+                    else:
+                        break
+            else:
+                code = 3
+                msg = 'password error'
+                data = []
+        cursor.close()
+        conn.close()
+        return jsonify(code=code, msg=msg, data=data)
+    else:
+        return "GET"
+
+
+@app.route('/upload_book/', methods=['GET', 'POST'])
+def upload_book():
+    if request.method == 'POST':
+        conn = sqlite3.connect("StudyWell.db")
+        book_name = str(request.form['book_name'])
+        author = str(request.form['author'])
+        publication = str(request.form['publication'])
+        description = str(request.form['description'])
+        publish_date = str(request.form['publish_date'])
+        book_file = request.files['book_file']
+        file_type = str(secure_filename(book_file.filename)).split('.')[-1]
+        cursor = conn.cursor()
+        id_list = cursor.execute("select book_id from book order by book_id desc ").fetchall()
+        max_id = id_list[0][0] + 1
+        upload_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        # path in server
+        path = "/www/wwwroot/book/" + str(max_id) + "_" + book_name + "." + file_type
+        # test in localhost
+        # path = str(max_id)+"_"+book_name+"."+file_type
+        book_file.save(path)
+        cursor.execute(
+            "insert into book(book_name,book_id,author,publication,publish_date,book_description,book_location,upload_date)"
+            "values (?,?,?,?,?,?,?,?)",
+            [book_name, max_id, author, publication, publish_date, description, path, upload_date, ])
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify(msg="upload successfully", code=1)
+    else:
+        return "GET"
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username_register = request.form['username']
+        password_register = request.form['password']
+        userphoto_register = request.files['user_photo']
+        connection = sqlite3.connect("StudyWell.db")
+        cursor = connection.cursor()
+        user_list = cursor.execute("select * from user where user.username = ?", [username_register, ]).fetchall()
+        if len(user_list) == 0:
+            file_type = str(secure_filename(userphoto_register.filename)).split('.')[-1]
+            # path in server
+            path = "/www/wwwroot/user_photo/" + str(username_register) + "." + file_type
+            # test in localhost
+            # path =  str(username_register) + "." + file_type
+            userphoto_register.save(path)
+            code = 1
+            msg = "register successfully"
+            cursor.execute("insert into user(username,password,image) values(?,?,?)",
+                           [username_register, password_register, path, ])
+            connection.commit()
+        else:
+            code = 0
+            msg = "user  already exists"
+        cursor.close()
+        connection.close()
+        return jsonify(code=code, msg=msg)
+    else:
+        return "only accept post method"
+
+
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+    if request.method == 'POST':
+        query_info = str(request.form['info'])
+        connection = sqlite3.connect("StudyWell.db")
+        cursor = connection.cursor()
+        book_list = []
+        list_1 = cursor.execute("select * from book where book.book_name like '%" + query_info + "%'").fetchall()
+        list_2 = cursor.execute("select * from book where book.book_id like '%" + query_info + "%'").fetchall()
+        list_3 = cursor.execute("select * from book where book.author like '%" + query_info + "%'").fetchall()
+        list_4 = cursor.execute("select * from book where book.publication like '%" + query_info + "%'").fetchall()
+        list_5 = cursor.execute("select * from book where book.publish_date like '%" + query_info + "%'").fetchall()
+        list_6 = cursor.execute("select * from book where book.book_description like '%" + query_info + "%'").fetchall()
+        list_7 = cursor.execute("select * from book where book.book_location like '%" + query_info + "%'").fetchall()
+        list_8 = cursor.execute("select * from book where book.upload_date like '%" + query_info + "%'").fetchall()
+        temp = [list_1, list_2, list_3, list_4, list_5, list_6, list_7, list_8]
+        for raw in temp:
+            book_list.extend(eval(str(raw)))
+        book_list = list(set(book_list))  # 去重
+        if len(book_list) == 0:
+            code = 0
+            msg = "cant find any books"
+            data = []
+            connection.commit()
+        else:
+            code = 1
+            msg = 'query success'
+            data = []
+            i = 0
+            for book in book_list:
+                if i < 10:
                     book_info = {
                         "book_id": book[1],
                         "book_name": book[0],
@@ -41,83 +163,13 @@ def login():
                         "upload_date": book[6],
                     }
                     data.append(book_info)
-            else:
-                status = '3'
-                msg = 'password error'
-                data = []
-        ans = {
-            "status": status,
-            "msg": msg,
-            "data": data
-        }
-
-        cursor.close()
-        conn.close()
-        return ans
-    else:
-        return "GET"
-
-
-@app.route('/upload_book/', methods=['GET', 'POST'])
-def upload_book():
-    if request.method == 'POST':
-        conn = sqlite3.connect("StudyWell.db")
-        book_name= str(request.form['book_name'])
-        author = str(request.form['author'])
-        publication = str(request.form['publication'])
-        description = str(request.form['description'])
-        publish_date = str(request.form['publish_date'])
-        book_file = request.files['book_file']
-        file_type = str(secure_filename(book_file.filename)).split('.')[-1]
-        cursor = conn.cursor()
-        id_list = cursor.execute("select book_id from book order by book_id desc ").fetchall()
-        max_id = id_list[0][0]+1
-        upload_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        # path in server
-        path ="/www/wwwroot/book/"+str(max_id)+"_"+book_name+"."+file_type
-        # test in localhost
-        #path = str(max_id)+"_"+book_name+"."+file_type
-        book_file.save(path)
-        cursor.execute("insert into book(book_name,book_id,author,publication,publish_date,book_description,book_location,upload_date)"
-                       "values (?,?,?,?,?,?,?,?)",[book_name,max_id,author,publication,publish_date,description,path,upload_date,])
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return jsonify(msg="upload successfully",code =1 )
-    else:
-        return "GET"
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username_register = request.form['username']
-        password_register = request.form['password']
-        userphoto_register =request.files['user_photo']
-        connection =sqlite3.connect("StudyWell.db")
-        cursor =connection.cursor()
-        user_list = cursor.execute("select * from user where user.username = ?",[username_register,]).fetchall()
-        msg = ""
-        code = 0
-        if len(user_list) == 0:
-            file_type = str(secure_filename(userphoto_register.filename)).split('.')[-1]
-            #path in server
-            path = "/www/wwwroot/user_photo/"+str(username_register)+"."+file_type
-            #test in localhost
-            #path =  str(username_register) + "." + file_type
-            userphoto_register.save(path)
-            code = 1
-            msg = "register successfully"
-            cursor.execute("insert into user(username,password,image) values(?,?,?)",[username_register,password_register,path,])
-            connection.commit()
-        else:
-            code = 0
-            msg = "user  already exists"
+                    i += 1
         cursor.close()
         connection.close()
-        return jsonify(code =code,msg = msg )
+        return jsonify(code=code, msg=msg, data=data)
     else:
         return "only accept post method"
+
 
 if __name__ == "__main__":
     app.run()
